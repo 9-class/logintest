b5ec357fbef76dc25b982ff5fc6626182c1ab50d
aurora <109873714+class-8@users.noreply.github.com>
2022-11-21 18:08:31 +0530

    hfhhfh

--- regdb.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `tbluser` (
-    `ID` int(10) NOT NULL,
-    `FullName` varchar(200) DEFAULT NULL,
-    `MobileNumber` bigint(10) DEFAULT NULL,
-    `Email` varchar(200) DEFAULT NULL,
-    `Password` varchar(200) DEFAULT NULL,
-    `RegDate` timestamp NULL DEFAULT current_timestamp()
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
